438126035af785f17ce8951127bac9c8335b4864
Dayana Lopez <dajhanalopez29@gmail.com>
2024-09-26 19:48:20 -0500

    Se crea el archivo OperadoresProject/OperadoresArimeticos.py

---
+++ operadoresProject/OperadoresAritmeticos.py
@@ -0,0 +1,33 @@
+#Ejemplo de operadores Aritmeticos
+#numero1:int = 18
+#numero2:int = 12
+numero1 = int(input("Ingrese el numero1: "))
+numero2 = int(input("Ingrese el numero2: "))
+suma = numero1 + numero2
+print(f"La suma de {numero1} y {numero2} es: {suma}")
+
+
+num1 = int(input("Ingrese el numero1: "))
+num2 = int(input("Ingrese el numero2: "))
+resta= num1 - num2
+print(f"La suma de {num1} y {num2} es: {resta}")
+
+
+num1 = int(input("Ingrese el numero1: "))
+num2 = int(input("Ingrese el numero2: "))
+resta= num1 - num2
+print(f"La suma de {num1} y {num2} es: {resta}")
+
+numer1 = int(input("Ingrese el numero1: "))
+numer2 = int(input("Ingrese el numero2: "))
+multiplicacion = numer1 * numer2
+print(f"La suma de {numer1} y {numer2} es: {multiplicacion}")
+
+nu1 = int(input("Ingrese el numero1: "))
+nu2 = int(input("Ingrese el numero2: "))
+modulo= nu1 % nu2
+print(f"La suma de {nu1} y {nu2} es: {modulo}")
+
+
+
+
