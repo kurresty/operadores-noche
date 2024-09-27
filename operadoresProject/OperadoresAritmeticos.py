3ae1363566674a408f6d1c4324e45beeadba65bf
Dayana Lopez <dajhanalopez29@gmail.com>
2024-09-26 20:50:01 -0500

    Se crea el archivo OperadoresProject/OperadoresAritmeticos.py

--- operadoresProject/OperadoresAritmeticos.py
+++ operadoresProject/OperadoresAritmeticos.py
@@ -4,9 +4,17 @@
 numero1 = int(input("Ingrese el numero1: "))
 numero2 = int(input("Ingrese el numero2: "))
 suma = numero1 + numero2
-resta = numero1 + numero2
-suma = numero1 + numero2
-suma = numero1 + numero2
-suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+modulo = numero1 % numero2
+divisionentera= numero1 // numero2
 print(f"La suma de {numero1} y {numero2} es: {suma}")
+print(f"La resta de {numero1} y {numero2} es: {resta}")
+print(f"La multiplicacion de {numero1} y {numero2} es: {multiplicacion}")
+print(f"La division de {numero1} y {numero2} es: {division}")
+print(f"La modulo de {numero1} y {numero2} es: {modulo}")
+print(f"La divisionentera de {numero1} y {numero2} es: {divisionentera}")
+
+
 
